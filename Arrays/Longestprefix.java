2792a1f2fbb90728d289808ae2808a8cac0ca0c5
sarthak <231179030+MustbeSarthak@users.noreply.github.com>
2026-03-28 19:48:48 +0530

    Fixed a Bug in Longestprefix

--- Arrays/Longestprefix.java
+++ Arrays/Longestprefix.java
@@ -1,36 +1,27 @@
-// LEETCODE 14 
-
-
-class Longestprefix{
+class Longestprefix {
 
     public static void main(String[] args) {
-        StringBuilder res = new StringBuilder();
 
-        String[] arr = {"flower", "flow", "char"};
+        StringBuilder res = new StringBuilder(); // Using string builder for better performance
+
+        String[] arr = {"flower", "flow", "fiar"};
         String base = arr[0];
-        for(int i = 0; i < arr.length ; i++){
-            char ch = base.charAt(i);
-            for(int j = 0; j < arr.length; j++){
-                if(i > arr[j].length() || arr[j].charAt(i) != ch){
-                    System.out.println(res.toString());
+
+        for (int i = 0; i < base.length(); i++) {
+
+            char ch = base.charAt(i); 
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (i >= arr[j].length() || arr[j].charAt(i) != ch) { // The condition in case out of bound error
+                    System.out.println(res.toString()); 
                     return;
                 }
-
             }
-            res.append(ch);
+
+            res.append(ch); // Returning the string 
         }
+
         System.out.println(res.toString());
     }
-    // static int findPrefix(int[] arr){
-    //     int count = 0;
-    //     for(int i = 0; i < arr.length ; i++){
-    //         for(int j = i + 1; arr.length ; j++){
-    //            if(arr[i].charAt(0) == arr[j].charAt(0)){
-    //             count++;
-    //            }
-    //     }
-    //     }
-    //     return 0;
-    // }
-    
 }
