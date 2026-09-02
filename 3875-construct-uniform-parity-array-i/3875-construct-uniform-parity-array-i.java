c446edf590358ecaaa3742c37637dc05303306fb
Sharthak Sharma <231179030+MustbeSarthak@users.noreply.github.com>
2026-09-02 22:31:20 +0530

    Time: 1 ms (31.89%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        boolean hasOdd = false;
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                hasOdd = true;
+                break;
+            }
+        }
+        if (!hasOdd) {
+            return true;
+        }
         return true;
     }
 }
